02b5ae4d4e253f59d2c53a7b44294a3893663ce5
Danila Kalinin <strannik.danil@gmail.com>
2020-11-15 19:31:01 +0700

    Initial migration created.

---
+++ TaskMaster/Migrations/20201115122720_InitialMigration.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskMaster.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Patronymic = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    CustomerCompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    ExecutorCompanyName = table.Column<string>(type: "TEXT", nullable: false),
+                    BegginingDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CompletionDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LeaderId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_Employees_LeaderId",
+                        column: x => x.LeaderId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeProject",
+                columns: table => new
+                {
+                    MembersId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProjectsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProject", x => new { x.MembersId, x.ProjectsId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Employees_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Projects_ProjectsId",
+                        column: x => x.ProjectsId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeProject_ProjectsId",
+                table: "EmployeeProject",
+                column: "ProjectsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_LeaderId",
+                table: "Projects",
+                column: "LeaderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeProject");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- TaskMaster/Services/DatabaseContext.cs
+++ TaskMaster/Services/DatabaseContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TaskMaster.Models;
 
 namespace TaskMaster.Services
@@ -12,15 +8,39 @@ namespace TaskMaster.Services
     /// </summary>
     public class DatabaseContext : DbContext
     {
-        public DbSet<Employee> Employees { get; set; }
+        #region Public Constructors
 
-        public DbSet<Project> Projects { get; set; }
-
+        /// <summary>
+        /// Creates database context and apply migrations if need.
+        /// </summary>
+        /// <param name="options">Database context options.</param>
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            Database.Migrate();
         }
 
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Database set of employees.
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+
+        /// <summary>
+        /// Database set of projects.
+        /// </summary>
+        public DbSet<Project> Projects { get; set; }
+
+        #endregion Public Properties
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Set up models relations params.
+        /// </summary>
+        /// <param name="modelBuilder">Model builder.</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Project>(projectModelBuilder =>
@@ -35,5 +55,7 @@ namespace TaskMaster.Services
                     .HasForeignKey(project => project.LeaderId);
             });
         }
+
+        #endregion Protected Methods
     }
 }
